98b94199d93e5b2b2e248a4731e216e8fa86de36
thaksaum <thaksaum@sheridancollege.ca>
2023-02-20 16:17:16 -0500

    lets go

---
+++ src/main/java/com/mycompany/practice/Practice.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practice;
+
+/**
+ *
+ * @author saumy
+ */
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    System.out.println("DEEP SIR");
+    }
+}
